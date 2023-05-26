92678c9b1a00f0b749e8eaa58a89309055366b86
yeakode <hiwrld1010@gmail.com>
2023-05-26 18:26:06 +0900

    stories

---
+++ stories/Dropdown.stories.js
@@ -0,0 +1,12 @@
+import { Dropdown } from "./Dropdown";
+
+export default {
+  title: "Dropdown",
+  component: Dropdown,
+};
+
+export const Default = {
+  args: {
+    list: ["OOO님, 안녕하세요", "상품 리스트 페이지", "북마크 페이지"],
+  },
+};
